efc71dbfbc4c3c09963b3875a3b19e94b4cc11d3
panda <p4nd4@posteo.de>
2024-01-05 12:10:52 +0100

    move data to child component

--- src/components/App.js
+++ src/components/App.js
@@ -2,40 +2,16 @@ import { useState } from "react";
 
 import Menu from "./Navigations/Menu";
 import Header from "./Header";
-import booksMockData from "../mocks/books";
 import { About } from "./About";
 import { Footer } from "./Footer";
 import { Books } from './Books/Books';
 
 function App() {
-  const [books, setBooks] = useState(booksMockData);
   const [isMenuOpen, setIsMenuOpen] = useState(false);
-  const [selectedFilter, setSelectedFilter] = useState("All");
 
   const toggleMenu = () => {
     setIsMenuOpen(!isMenuOpen);
   };
-
-  const selectFilter = (filter) => {
-    setSelectedFilter(filter);
-    setBooks(
-      filter === "All"
-        ? booksMockData
-        : booksMockData.filter((book) => book.category === filter)
-    );
-  };
-
-  const filters = ["All", "Design", "Mobile", "DevOps", "Essentials"];
-
-  const tabItems = filters.map((filter) => (
-    <li
-      className={filter === selectedFilter ? "active" : ""}
-      key={filter}
-      onClick={() => selectFilter(filter)}
-    >
-      <a href="#0">{filter}</a>
-    </li>
-  ));
 
   return (
     <div id="page-wrap">
@@ -60,6 +36,7 @@ function App() {
               <button onClick={toggleMenu} className="btn btn-lg btn-outline">
                 <i className="fa fa-graduation-cap" /> <span>Training</span>
               </button>
+
             </li>
           </ul>
         </div>
@@ -67,11 +44,7 @@ function App() {
 
       <Header title="By React" />
 
-      <Books
-      books={books}
-      tabItems={tabItems}
-      selectedFilter={selectedFilter}
-      />
+      <Books />
 
       <About />
       <Footer />
--- src/components/Books/Books.js
+++ src/components/Books/Books.js
@@ -1,7 +1,32 @@
 import { BookList } from "./BookList";
 import { BooksFilter } from "./BookFilter";
+import { useState } from "react";
+import booksMockData from "../../mocks/books.js";
 
-export function Books({books, tabItems, selectedFilter}) {
+export function Books() {
+  const [books, setBooks] = useState(booksMockData);
+  const [selectedFilter, setSelectedFilter] = useState("All");
+
+  const selectFilter = (filter) => {
+    setSelectedFilter(filter);
+    setBooks(
+      filter === "All"
+        ? booksMockData
+        : booksMockData.filter((book) => book.category === filter)
+    );
+  };
+
+  const filters = ["All", "Design", "Mobile", "DevOps", "Essentials"];
+
+  const tabItems = filters.map((filter) => (
+    <li
+      className={filter === selectedFilter ? "active" : ""}
+      key={filter}
+      onClick={() => selectFilter(filter)}
+    >
+      <a href="#0">{filter}</a>
+    </li>
+  ));
 
   return (
     <section id="books">
